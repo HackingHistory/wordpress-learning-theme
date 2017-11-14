dceb446f8d282b9a8914aab0e1270a993e810ad6
Tomi <tomi.katona@mail.utoronto.ca>
2017-11-13 20:17:21 -0500

    f

--- footer.php
+++ footer.php
@@ -4,10 +4,19 @@
   <h1>Majestic Peak Foundation</h1>
   <p>University of Toronto | 27 King's College Circle Toronto, Ontario M5S 1A1 Canada</p>
   </div>
+
+  <div class="twitter">
+  <h1>Our Twitter Feed</h1>
+<a class="twitter-timeline" href="https://twitter.com/TwitterDev/lists/national-parks?ref_src=twsrc%5Etfw">A Twitter List by TwitterDev</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+  </div>
+
+
   <div class="donate">
   <h1>Donate</h1>
   <p>Please donate here</p>
   </div>
+
+
 </footer>
 <!-- do some Wordpress magic -->
 <?php wp_footer(); ?>
