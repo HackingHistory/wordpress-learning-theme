e30379cb9f0305b48bbf0a347b469790bfe1f42a
sol-monica <32038022+sol-monica@users.noreply.github.com>
2017-11-13 22:49:05 -0500

    basic title changes, checking that site updates

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 <!-- make an ugly footer -->
 <footer id="footer">
-  <h1>FOOTER</h1>
-  <p>Powered by madness</p>
+  <h1>HIS455 STA03</h1>
+  <p>By Monica</p>
 </footer>
 <!-- do some Wordpress magic -->
 <?php wp_footer(); ?>
