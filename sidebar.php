47fbe22b0b3ac9feb2c9b5611a3921c3c01db49b
dangelan <31895867+dangelan@users.noreply.github.com>
2017-11-19 21:17:54 -0500

    added links to resources

--- sidebar.php
+++ sidebar.php
@@ -10,7 +10,12 @@
 
 	<div id="sidebar">
             <h2 class="sidebartitle">Other Resources</h2>
-            <h3> UNESCO </h3>       
+            <h3> UNESCO </h3>
+            <link> http://whc.unesco.org/en/list/1513 </link>
+            <h3> FACEBOOK </h3>
+            <link> https://www.facebook.com/pages/Khangchendzonga-National-Park/103776976328344 </link>
+            <h3> TRIP ADVISOR </h3>
+            <link> https://www.tripadvisor.ca/ShowUserReviews-g659796-d1764036-r344922996-Khangchendzonga_Biosphere_Reserve-Gangtok_East_Sikkim_Sikkim.html </link>
         </div>
 
     <?php endif; ?>
