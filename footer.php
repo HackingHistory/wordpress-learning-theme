7b284533e20de3f57a0dec60346f83796cd2e9ad
WhitePeachTea <peng_lei_1021@hotmail.com>
2017-11-21 18:46:17 -0500

    Fixed buttons

--- footer.php
+++ footer.php
@@ -1,6 +1,14 @@
 <footer id="footer">
   <h1><img src="https://www.karelcapek.co.jp/html/template/default/images/common/logo.png"></h1>
   <p>Peng Lei</p>
+  <!-- Add icon library -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+  <!-- Add font awesome icons -->
+  <a href="https://www.facebook.com/karelcapek.teatime/" class="fa fa-facebook"></a>
+  <a href="https://twitter.com/karelabuzzy" class="fa fa-twitter"></a>
+  <a href="https://www.instagram.com/karelcapek_official/" class="fa fa-instagram" ></a>
+  
 </footer>
 <?php wp_footer(); ?>
 
