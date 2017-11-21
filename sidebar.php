963d5e16c8b6f10464d695458a4d82afcbdb9110
WhitePeachTea <peng_lei_1021@hotmail.com>
2017-11-21 01:44:04 -0500

    sidebar 1

--- sidebar.php
+++ sidebar.php
@@ -7,9 +7,12 @@
 	<?php else : ?>
 
 		<div id="sidebar">
-          <h2 class="sidebartitle">Here is the Sidebar</h2>          
+          <h2 class="sidebartitle">Do you know?</h2>
+					<p>
+						<a href = https://en.wikipedia.org/wiki/Karel_%C4%8Capek>Karel Čapek</a> in Roman letter is also the name for a Czech writer of the early 20th century.
+					</p>
         </div>
 
-	<?php endif; ?>	
+	<?php endif; ?>
 
 </div>
