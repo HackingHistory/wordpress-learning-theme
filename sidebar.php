d271a4c828b96886a8dfbe5af8c7f5496abc8ee1
WhitePeachTea <peng_lei_1021@hotmail.com>
2017-11-21 18:17:36 -0500

    sidebar

--- sidebar.php
+++ sidebar.php
@@ -11,6 +11,11 @@
 					<p>
 						<a href = https://en.wikipedia.org/wiki/Karel_%C4%8Capek>Karel Čapek</a> in Roman letter is also the name for a Czech writer of the early 20th century.
 					</p>
+
+					<h3 class = "sidebartitle">店舗のご紹介</h3>
+						<p>
+							We have several shops across Japan. More information could be found <a href = https://www.karelcapek.co.jp/user_data/shop#kichi>here</a>.
+						</p>
         </div>
 
 	<?php endif; ?>
