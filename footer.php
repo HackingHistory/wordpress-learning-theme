f68b5f300b846e410c1a2e36ebb5b26a602791ba
WhitePeachTea <peng_lei_1021@hotmail.com>
2017-11-21 01:59:29 -0500

    2

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
 <footer id="footer">
-  <h1><img src="https://www.karelcapek.co.jp/html/template/default/images/common/logo.png" alt="Smiley face" height="42" width="42"></h1>
+  <h1><img src="https://www.karelcapek.co.jp/html/template/default/images/common/logo.png"></h1>
   <p>Peng Lei</p>
 </footer>
 <?php wp_footer(); ?>
