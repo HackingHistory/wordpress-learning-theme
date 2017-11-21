17e91a0a28c330ff70ca91f0a1969f7619008bc9
aadran <32490261+aadran@users.noreply.github.com>
2017-11-20 20:26:23 -0500

    4

--- sidebar.php
+++ sidebar.php
@@ -11,6 +11,7 @@
 	<div id="sidebar">
             <h2 class="sidebartitle">Here is the Sidebar</h2>
 <p>The mountain chains which run southwards from the main Himalayan range form the natural boundaries of Sikkim; the Chola range dividing it from Tibet in the Northeast and Bhutan in the Southeast, the Singalila range separating it from Nepal in the West with the Greater Himalayan range forming the barrier between Sikkim and Tibet in the North.
+  
 Floating high over the cloud-covered lower Himalaya, Mt. Khangchendzonga dominates the landscape of Sikkim. At 27,803 ft is the third highest mountain in the world and the highest in India.</p>
         </div>
 
